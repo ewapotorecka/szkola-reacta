95c73f2255c2d7ed64d03ec7f843bcdc69be6481
Ewa Potorecka <ewa.potorecka@gmail.com>
2020-10-30 11:15:16 +0100

    Deleted serviceWorker

---
+++ exercises/week3/magicButton/button/src/App.js
@@ -0,0 +1,13 @@
+import { Textarea, Input, Button } from './components/Form';
+
+function App() {
+  return (
+    <div className="App">
+		<Button/>
+		<Input/>
+		<Textarea/>
+    </div>
+  );
+}
+
+export default App;
---
+++ exercises/week3/magicButton/button/src/colors/Colors.js
@@ -0,0 +1,13 @@
+const colorPalette = {
+	orchidOrange: '#FEA47F',
+	spiroDiscoBall: '#25CCF7',
+	honeyGlow: '#EAB543',
+	sweetGarden: '#55E6C1',
+	fallingStar: '#CAD3C8',
+	shipsOfficer: '#2C3A47',
+	fieryFuchsia: '#B33771',
+	highlighterLavender: '#82589F',
+	endingNavyBlue: '#182C61'
+};
+
+export default colorPalette;
